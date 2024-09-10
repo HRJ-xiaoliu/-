61422088c92be03629fabd1c732c075fcb6728d2
Kliealy <482165350@qq.com>
2024-09-10 21:33:49 +0800

    910

--- 学习/test.py
+++ 学习/test.py
@@ -1,23 +1,9 @@
-a = 21
-b = 10
-c = 0
+#!/usr/bin/python
  
-if ( a == b ):
-   print ("1 - a 等于 b")
-else:
-   print ("1 - a 不等于 b")
+count = 0
+
+while (count < 9):
+   print ('The count is:'), count
+   count = count + 1
  
-if ( a != b ):
-   print ("2 - a 不等于 b")
-else:
-   print ("2 - a 等于 b")
- 
-if ( a < b ):
-   print ("3 - a 小于 b")
-else:
-   print ("3 - a 大于等于 b")
- 
-if ( a > b ):
-   print ("4 - a 大于 b")
-else:
-   print ("4 - a 小于等于 b")
+print ("Good bye!")
